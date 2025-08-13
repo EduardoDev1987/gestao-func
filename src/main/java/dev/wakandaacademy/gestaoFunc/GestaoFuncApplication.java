8cd10083c5851e91fb4a97efe67e8f843d34dd88
Eduado Silva <eduardops87@gmail.com>
2025-08-13 16:33:38 -0300

    init comit

---
+++ src/main/java/dev/wakandaacademy/gestaoFunc/GestaoFuncApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.gestaoFunc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFuncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/gestaoFunc/GestaoFuncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.gestaoFunc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
